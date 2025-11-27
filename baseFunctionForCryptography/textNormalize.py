81911829f1d84045660afeed1ed142b29e41d4e7
Alex <alexyulyadkov@gmail.com>
2025-11-27 19:54:48 +0300

    upgrade atbash

--- baseFunctionForCryptography/textNormalize.py
+++
@@ -1,3 +0,0 @@
-def normalize_text(text: str = "Не все те повара, что с длинными ножами ходят.") -> str:
-    norm_text = text.replace(" ", "").lower().replace(",", "зпт").replace(".", "тчк")
-    return norm_text
