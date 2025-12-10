e2e5f6c314d97510eafa4142ad5670a616b7366c
Alex <alexyulyadkov@gmail.com>
2025-12-11 02:36:09 +0300

    added analitic bigramm

--- part1/classical_Caesar_cipher.py
+++ part1/classical_Caesar_cipher.py
@@ -4,13 +4,13 @@ def encoder(text):
     i = 0
     for sim in text:
         if sim in alf:
-            idx = (alf.index(sim) - 3) % len(alf)
+            idx = (alf.index(sim) + 3) % len(alf)
             res += alf[idx]
             i += 1
         if i % 5 == 0:
             res += " "
-    
+
     return res
 
-res = encoder("хиути рлицд легих диффл оярсж слзио гихеи олнлп флояр сжсхъ н")
+res = encoder("безопасность")
 print(res)
